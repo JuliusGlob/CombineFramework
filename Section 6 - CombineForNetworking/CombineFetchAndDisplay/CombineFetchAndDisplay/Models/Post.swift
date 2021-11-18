aa4754f61a934b1be4cae425aefd19ca3c784839
JuliusGlob <julio.robles@globant.com>
2021-11-18 11:08:23 -0300

    section 6

---
+++ Section 6 - CombineForNetworking/CombineFetchAndDisplay/CombineFetchAndDisplay/Models/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  CombineFetchAndDisplay
+//
+//  Created by Julio Ismael Robles on 18/11/2021.
+//
+
+import Foundation
+
+struct Post: Decodable {
+    let title: String
+    let body: String
+}
